edd5d6734504450acf245ce72cacdde5019d3a7c
v-daca <106557021+v-daca@users.noreply.github.com>
2023-05-16 08:43:22 -0600

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import bottle
 from bottle import route, run, Response, template
 import json
 import image
+import paste
 
 app = bottle.app()
 
@@ -17,6 +18,6 @@ def index():
     return template('index.tpl',data="Request completed!", title=title)
 
 if __name__ == '__main__':
-	run(app, host='0.0.0.0', port=8000, debug=True, reloader=True)
+	run(server='paste', app, host='0.0.0.0', port=8000, debug=True, reloader=True)
 	
 #serverApp = bottle.default_app()
