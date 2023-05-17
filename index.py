32a9f4947cbb7829057aeec2ecd27bb74527bf01
v-daca <106557021+v-daca@users.noreply.github.com>
2023-05-17 10:41:22 -0600

    Update index.py

--- index.py
+++ index.py
@@ -2,19 +2,22 @@ import bottle
 from bottle import route, run, Response, template
 import json
 import image
+from viztracer import VizTracer
 
-def call_service():
-    directoryName = 'photos'
-    image.process(directoryName)
-
-@route('/')
-def index():
-    """Home page"""
-    title = "Image Processor App"
-    call_service()
-    return template('index.tpl',data="Request completed!", title=title)
+with VizTracer(output_file="optional.json") as tracer:
+	
+	def call_service():
+		directoryName = 'photos'
+		image.process(directoryName)
+	
+	@route('/')
+	def index():
+		"""Home page"""
+		title = "Image Processor App"
+		call_service()
+		return template('index.tpl',data="Request completed!", title=title)
 
 #if __name__ == '__main__':
 #	run(host='0.0.0.0', port=8000, debug=False, reloader=True)
 	
-serverApp = bottle.default_app()
+	serverApp = bottle.default_app()
