29641fcf3e0e6afdef92e315942d18a1fc7220ba
v-daca <106557021+v-daca@users.noreply.github.com>
2023-05-17 00:04:08 -0600

    Update index.py

--- index.py
+++ index.py
@@ -17,4 +17,4 @@ def index():
     return template('index.tpl',data="Request completed!", title=title)
 
 if __name__ == '__main__':
-	run(app, host='0.0.0.0', port=8000, debug=True, reloader=True)
+	run(app, host='0.0.0.0', port=8000, debug=False, reloader=True)
