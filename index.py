ce2ff4981b0cb5e3011c679f64d37de36ad418f3
v-daca <106557021+v-daca@users.noreply.github.com>
2023-05-17 00:30:35 -0600

    Update index.py

--- index.py
+++ index.py
@@ -2,22 +2,16 @@ import bottle
 from bottle import route, run, Response, template
 import json
 from image import process
-import concurrent.futures
 
 app = bottle.app()
 
-def call_service():
-	directoryName = 'photos'
-	with concurrent.futures.ThreadPoolExecutor() as executor:
-		executor.submit(process, directoryName)
-	#image.process(directoryName)
-
 @app.route('/')
 def index():
-    """Home page"""
-    title = "Image Processor App"
-    call_service()
-    return template('index.tpl',data="Request completed!", title=title)
+	directoryName = 'photos'
+	"""Home page"""
+	title = "Image Processor App"
+	process(directoryName)
+	return template('index.tpl',data="Request completed!", title=title)
 
 if __name__ == '__main__':
 	run(app, host='0.0.0.0', port=8000, debug=False, reloader=True)
