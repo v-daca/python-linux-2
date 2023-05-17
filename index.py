c75242ee7b0d3344d5443499abc22ec9c2579074
v-daca <106557021+v-daca@users.noreply.github.com>
2023-05-17 01:08:32 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,20 @@
 import bottle
 from bottle import route, run, Response, template
 import json
-from image import process
+import image
 
-app = bottle.app()
+def call_service():
+    directoryName = 'photos'
+    image.process(directoryName)
 
-@app.route('/')
+@route('/')
 def index():
-	directoryName = 'photos'
-	"""Home page"""
-	title = "Image Processor App"
-	process(directoryName)
-	return template('index.tpl',data="Request completed!", title=title)
+    """Home page"""
+    title = "Image Processor App"
+    call_service()
+    return template('index.tpl',data="Request completed!", title=title)
 
 if __name__ == '__main__':
-	run(app, host='0.0.0.0', port=8000, debug=False, reloader=True)
+	run(host='0.0.0.0', port=8000, debug=False, reloader=True)
+	
+serverApp = bottle.default_app()
