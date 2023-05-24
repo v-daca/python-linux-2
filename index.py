d1661400c1f290d4d93621123b4834b07236c328
v-daca <106557021+v-daca@users.noreply.github.com>
2023-05-24 16:48:28 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import bottle
 from bottle import route, run, Response, template
 import json
 import image
@@ -14,16 +15,19 @@ def index():
 	title = "Image Processor App"
 	call_service()
 	return template('index.tpl',data="Request completed!", title=title)
-pr = cProfile.Profile()
-pr.enable()
 
-result = index()
-
-pr.disable()
-
-s = io.StringIO()
-sortby = SortKey.CUMULATIVE
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-ps.print_stats()
-print(s.getvalue())
-serverApp = bottle.default_app()
+if __name__ == "__main__":
+	pr = cProfile.Profile()
+	pr.enable()
+	
+	result = index()
+	
+	pr.disable()
+	
+	s = io.StringIO()
+	sortby = SortKey.CUMULATIVE
+	ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+	ps.print_stats()
+	print(s.getvalue())
+	
+	serverApp = bottle.default_app()
